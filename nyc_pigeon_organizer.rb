318c3a52b368fae864ea9b09229fe7857bdd1b19
Joseph P <jpark041@gmail.com>
2019-07-15 04:48:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,42 @@
 def nyc_pigeon_organizer(data)
-  new_hash = {}
+  # new_hash = {}
 
-  data.each do |property, hash|
-    hash.each do |attribute, array|
-      array.each do |name|
-        if !new_hash.has_key?(name)
+  # data.each do |property, hash|
+  #   hash.each do |attribute, array|
+  #     array.each do |name|
+  #       if !new_hash.has_key?(name)
+  #         new_hash[name] = {}
+  #       end
+
+  #       if !new_hash[name].has_key?(property)
+  #         new_hash[name][property] = []
+  #       end
+
+  #       if !new_hash[name][property].include?(attribute)
+  #         new_hash[name][property] << attribute.to_s
+  #       end
+  #     end
+  #   end
+  # end
+  # new_hash
+  
+  #official solution
+  new_hash = {}
+  
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
           new_hash[name] = {}
         end
 
-        if !new_hash[name].has_key?(property)
-          new_hash[name][property] = []
+        if !new_hash[name][key]
+          new_hash[name][key] = []
         end
 
-        if !new_hash[name][property].include?(attribute)
-          new_hash[name][property] << attribute.to_s
-        end
+        new_hash[name][key] << new_value.to_s
+
       end
     end
   end
