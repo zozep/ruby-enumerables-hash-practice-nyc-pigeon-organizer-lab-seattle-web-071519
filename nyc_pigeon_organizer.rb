4695f89a0acc7e4f2ff940b32aeac74b8e76637d
Joseph P <jpark041@gmail.com>
2019-07-15 04:47:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,4 +19,25 @@ def nyc_pigeon_organizer(data)
   #   end
   # end
   # new_hash
+  
+  
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
+    end
+  end
+  new_hash
 end
