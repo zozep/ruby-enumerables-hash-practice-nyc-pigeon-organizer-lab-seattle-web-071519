2cfa518c672a835d8719a541b19b8edb22ef06aa
Joseph P <jpark041@gmail.com>
2019-07-15 04:47:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,22 @@
 def nyc_pigeon_organizer(data)
-  new_hash = {}
+  # new_hash = {}
 
-  data.each do |property, hash|
-    hash.each do |attribute, array|
-      array.each do |name|
-        if !new_hash.has_key?(name)
-          new_hash[name] = {}
-        end
+  # data.each do |property, hash|
+  #   hash.each do |attribute, array|
+  #     array.each do |name|
+  #       if !new_hash.has_key?(name)
+  #         new_hash[name] = {}
+  #       end
 
-        if !new_hash[name].has_key?(property)
-          new_hash[name][property] = []
-        end
+  #       if !new_hash[name].has_key?(property)
+  #         new_hash[name][property] = []
+  #       end
 
-        if !new_hash[name][property].include?(attribute)
-          new_hash[name][property] << attribute.to_s
-        end
-      end
-    end
-  end
-  new_hash
+  #       if !new_hash[name][property].include?(attribute)
+  #         new_hash[name][property] << attribute.to_s
+  #       end
+  #     end
+  #   end
+  # end
+  # new_hash
 end
